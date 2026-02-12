d72ddfea666d13997bb17ed541149016b8cc547d
Guilherme Cesar dos Santos Fina <guilhemefina498@gmail.com>
2026-02-12 00:56:09 -0300

    fiz o botão de voltar na animação

--- js/animacao.js
+++ js/animacao.js
@@ -1,41 +1,91 @@
 
 
-function animar ({
-    botaoId, totalPassos, totalFrames
+function animar_frente ({
+    idBotao, totalPassos, framesPorPasso, duracao = 1
 }) {
-    const botao = document.getElementById(botaoId)
-    const img = botao.parentElement.querySelector("img")
+    const botao = document.getElementById(idBotao)
+    const img = botao.parentElement.querySelector('img')
 
-    let frame = 1
     let intervalo = null
 
-
-    function tocar() {
+    function tocar () {
         if (botao.classList.contains('tocando')) return
+        if (img.dataset.passo == "final") return
 
         botao.classList.add('tocando')
-        let passo = Number(botao.dataset.passo)
+
+        let frame = 1
+        let passoAtual = Number(img.dataset.passo)
 
         intervalo = setInterval(() => {
+            
+            let indice = frame.toString().padStart(2, '0')
 
-            let indice = frame.toString().padStart(2, "0");
-            img.src = `../assets/imagens/sla/passo${passo}/frame_${indice}.png`
+            img.src = `../assets/imagens/sla/passo${passoAtual}/frame_${indice}.png`
+
             frame++
 
-            if (frame > totalFrames) {
+            if (frame > framesPorPasso) {
                 clearInterval(intervalo)
-                frame = 1
                 botao.classList.remove('tocando')
 
-                if (passo < totalPassos) {
-                    botao.dataset.passo++
+                if (passoAtual < totalPassos) {
+                    img.dataset.passo++
                 }
             }
-        }, 50)
 
-        
-        
+            botao.parentElement.querySelector('figcaption').innerText(passoAtual)
+
+        }, duracao * 1000 / framesPorPasso);
+
     }
 
+    botao.addEventListener('click', tocar)
+}
+
+function animar_tras ({
+    idBotao, framesPorPasso, duracao = 1
+}) {
+
+    const botao = document.getElementById(idBotao)
+    img = botao.parentElement.querySelector('img')
+
+    intervalo = null
+
+    function tocar () {
+
+        if (botao.classList.contains('tocando')) return
+        if (img.dataset.passo == 'inicio') return
+
+        botao.classList.add("tocando")
+
+        let frame = 1
+        passoAtual = Number(img.dataset.passo)
+
+        intervalo = setInterval(() => {
+            
+            let indice = (framesPorPasso - frame + 1).toString().padStart(2, '0')
+
+            img.src = `../assets/imagens/sla/passo${passoAtual}/frame_${indice}.png`
+
+            frame++
+
+            if (frame > framesPorPasso) {
+                clearInterval(intervalo)
+                botao.classList.remove("tocando")
+
+                if (passoAtual > 1) {
+                    img.dataset.passo--
+                }
+            
+            botao.parentElement.querySelector('figcaption').innerText(passoAtual)
+            }
+
+
+        }, duracao * 1000 / framesPorPasso);
+
+    }
+    
     botao.addEventListener("click", tocar)
+
 }
