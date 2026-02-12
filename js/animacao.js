9be4110ab32a0cd76a555d7cc83adc8c2705543a
Guilherme Cesar dos Santos Fina <guilhemefina498@gmail.com>
2026-02-12 02:08:01 -0300

    Comentei um pouco

--- js/animacao.js
+++ js/animacao.js
@@ -3,35 +3,53 @@
 function animar_frente ({
     idBotao, totalPassos, framesPorPasso, duracao = 1
 }) {
+    // Pegar o botão e a imagem
     const botao = document.getElementById(idBotao)
     const img = botao.parentElement.querySelector('img')
 
+    // Cria a variável do intervalo
     let intervalo = null
 
+    // Função que será chamada quando o botão for precionado
     function tocar () {
+
+        // Impede de começar uma animação antes que a anterior termine
         if (botao.classList.contains('tocando')) return
 
+        // Verifica se é uma animação válida de acordo com o passo atual
         let passoAtual = Number(img.dataset.passo)
         if (passoAtual > totalPassos) return
 
+        // Define o começo da animação
         botao.classList.add('tocando')
         let frame = 1
 
+        // Loop
         intervalo = setInterval(() => {
             
+            // Transforma o frame em um índice válido de acordo com o nome do arquivo
             let indice = frame.toString().padStart(2, '0')
+            // Muda a imagem
             img.src = `../assets/imagens/sla/passo${passoAtual}/frame_${indice}.png`
+            // muda o índice do frame
             frame++
 
+            // Checa para ver se chegou ao último frae
             if (frame > framesPorPasso) {
+                // Termina o loop
                 clearInterval(intervalo)
+
+                // Termina a animação
                 botao.classList.remove('tocando')
 
+                // Verifica se podemos ir ao próximo passo ou se chegamos ao último
                 if (passoAtual <= totalPassos) {
                     img.dataset.passo++
                 }
             }
-        }, duracao * 1000 / framesPorPasso);
+        },
+        // duração de cada animação em segundos
+        duracao * 1000 / framesPorPasso);
     }
     botao.addEventListener('click', tocar)
 }
