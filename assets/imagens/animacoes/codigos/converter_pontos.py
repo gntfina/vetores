dd15a0e5178d7ae4692b9a14490a2ec7b5b9011a
Guilherme Cesar dos Santos Fina <guilhemefina498@gmail.com>
2026-02-19 22:37:54 -0300

    Atualizei para já gerar as linhas do ggb

--- assets/imagens/animacoes/codigos/converter_pontos.py
+++ assets/imagens/animacoes/codigos/converter_pontos.py
@@ -1,12 +1,13 @@
 from vetor3d import Vetor3D as Vetor
+from string import ascii_lowercase
 
-def projecaoDoPontoNoPlano(posCamera: Vetor, dirCamera: Vetor, upCamera: Vetor, distPlano: float, ponto: Vetor) -> tuple[float, float] | None:
+def projecaoDoPontoNoPlano(posCamera: Vetor, dirCamera: Vetor, upCamera: Vetor, ponto: Vetor) -> tuple[float, float] | None:
     dirCamera = dirCamera.normalizar()
     
     eixoX = Vetor.produtoVetorial(dirCamera, upCamera).normalizar()
     eixoY = Vetor.produtoVetorial(eixoX, dirCamera).normalizar()
 
-    centroPlano = posCamera + dirCamera * distPlano
+    centroPlano = posCamera + dirCamera
     coefLin = Vetor.produtoEscalar(dirCamera, centroPlano)
 
     t_numerador = coefLin - Vetor.produtoEscalar(dirCamera, posCamera)
@@ -28,14 +29,58 @@ def projecaoDoPontoNoPlano(posCamera: Vetor, dirCamera: Vetor, upCamera: Vetor, 
 
     return (round(x, 2), round(y, 2))
 
+
+def magica(posCameras: Vetor, dirCameras: Vetor, upCameras: Vetor, pontos: Vetor) -> None:
+
+    print('\n')
+    # Cria a lista de pontos
+    texto = []
+    for ponto in pontos:
+        for indice, camera in enumerate(posCameras):
+            texto.append(str(projecaoDoPontoNoPlano(camera, dirCameras[indice], upCameras[indice], ponto)))
+    print('{' + ', '.join(texto) + '}')
+
+    print('\n')
+    # Cria a interpolação entre os pontos
+    letras = list(ascii_lowercase)
+    letras.remove('x')
+    letras.remove('y')
+    letras.remove('z')
+
+    qtd = len(posCameras)
+    texto = f'l1({qtd} k - {qtd - 1})'
+    for k in range(qtd - 1):
+        texto = f'(1 - {letras[k]}) (' + texto + f') + {letras[k]} l1({qtd} k - {1-k})'
+    texto = 'Sequence(' + texto + f', k, 1, {len(pontos)})'
+    print(texto)
+
+    print('\n')
+    # Cria as bases vetoriais
+    for k in range(3):
+        print(f'Vector(l2({2 * k + 2}), l2({2 * k + 1}))\n')
+
+    print('\n')
+    # Cria o plano
+    texto = []
+    for i in range(2):
+        for j in range(2):
+            texto.append(f'l2({i + 1}) + l2({j + 3})')
+    texto.insert(-1, texto.pop(-1))
+    print('Polygon({' + ', '.join(texto) + '})')
+
+    print('\n')
+    # Cria um lista com os pontos restantes
+    print('pontos = Take(l2, 7, length(l2))\n')
+
+
 distCamera = 5
-cameras = [
+posCameras = [
     distCamera * Vetor(.6, -.8, .5).normalizar(), 
     distCamera * Vetor.i(),
     - distCamera * Vetor.j()
 ]
-upCamera = Vetor.k()
-distPlano = 1
+dirCameras = [ -camera for camera in posCameras]
+upCameras = [Vetor.k()] * len(posCameras)
 pontos = [
     2 * Vetor.i(),
     - 2 * Vetor.i(),
@@ -44,12 +89,9 @@ pontos = [
     4 * Vetor.k(),
     - Vetor.k(),
     Vetor(-1, 1, 2),
-    Vetor(3, -3, 1)
+    Vetor(3, -3, 1),
+    Vetor(-1, 1, 0),
+    Vetor(3, -3, 0)
 ]
 
-
-for ponto in pontos:
-    lista = []
-    for camera in cameras:
-        lista.append(projecaoDoPontoNoPlano(camera, - camera, upCamera, distPlano, ponto))
-    print(lista)
+magica(posCameras, dirCameras, upCameras, pontos)
